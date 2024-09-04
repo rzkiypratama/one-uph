d69f8fc695431144d5af7ac3edc1f1346f9e37f1
rzkiypratama <rzkiypratama>
2024-09-04 16:12:09 +0800

    feat: add more props for fully customized cards

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,12 +5,17 @@ interface CardProps {
   title: string;
   height: string;
   width: string;
+  icons?: string;
   backgroundPosition: string;
+  gradientPosition: string;
+  gradientExtraPosition?: string;
+  gradientColor: string;
+  fontColor?: string;
   onClick?: () => void;
   isDimmed?: boolean;
 }
 
-const Card: React.FC<CardProps> = ({ backgroundImage, title, height, width, backgroundPosition, onClick, isDimmed }) => {
+const Card: React.FC<CardProps> = ({ backgroundImage, title, height, width, icons, backgroundPosition, gradientPosition, gradientExtraPosition, gradientColor, fontColor, onClick, isDimmed }) => {
   return (
     <div
       className="relative rounded-lg overflow-hidden cursor-pointer tracking-wider group font-montserrat"
@@ -25,11 +30,19 @@ const Card: React.FC<CardProps> = ({ backgroundImage, title, height, width, back
       onClick={onClick}
     >
       {/* Gradient Overlay */}
-      <div className={`absolute inset-0 bg-gradient-to-t from-[#BCD1FF] to-[#BCD1FF]/20 group-hover:from-[#BCD1FF] group-hover:to-[#BCD1FF]/50 ${isDimmed ? 'bg-[#F1F1F1] opacity-50' : ''}`}></div>
+      <div className={`absolute inset-0 from-[#BCD1FF] to-[#BCD1FF]/20 group-hover:from-[#BCD1FF] group-hover:ease-in-out ${isDimmed ? 'bg-[#F1F1F1] opacity-50' : ''}`}
+      style={{
+        backgroundImage: `linear-gradient(to ${gradientPosition}, ${gradientColor}, #f1f1f122)`, // default color #BCD1FF
+        transition: 'background-image 0.3s ease', 
+      }}
+      ></div>
 
       {/* Title */}
       <div className="absolute bottom-0 left-0 p-4">
-        <h3 className="text-main-font text-2xl w-32 group-hover:text-white">{title}</h3>
+        {icons && (
+          <img src={icons} alt="icon" className="w-30 mb-2" />
+        )}
+        <h3 className={`text-${fontColor} text-xl w-40 group-hover:text-white`}>{title}</h3>
       </div>
     </div>
   );
